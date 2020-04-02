8685df1810eaba5214b63364320fcb0973397f7a
apereira123456 <apereira123456@users.noreply.github.com>
2020-04-02 19:46:45 -0400

    Create opticalPattern3.m

---
+++ opticalPattern3.m
@@ -0,0 +1,22 @@
+function imageOut = opticalPattern3(m,n)
+
+    matrix1 = zeros(m,n,3);                 % Generate an m x n x 3 matrix of zeros
+    
+    matrix1(1:2:end,1:2:end,2) = 255;       % Odd rows and odd columns
+    matrix1(1:2:end,1:2:end,3) = 255;
+    
+    matrix1(2:2:end,2:2:end,2) = 255;       % Even rows and even columns      
+    
+    matrix1(1:2:end,2:2:end,3) = 255;       % Odd rows and even columns
+    
+    matrix1(2:2:end,1:2:end,1) = 255;       % Even rows and odd columns
+    
+    imageOut = uint8(matrix1);              % Convert from double to uint8
+    
+    figure
+    image(imageOut);
+
+    figure
+    imshow(imageOut);
+    
+end
